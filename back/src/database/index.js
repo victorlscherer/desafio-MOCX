16798bd68c54fd16fff38ad46aac969063ec45e1
Victor Scherer <victor_scherer@hotmail.com>
2022-06-14 09:55:56 -0300

    fix: code adaptive for docker

--- back/src/database/index.js
+++ back/src/database/index.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-mongoose.connect('mongodb://localhost/mocx_teste');
+mongoose.connect('mongodb://host.docker.internal:27017/mocx_teste');
 
 export default mongoose;
